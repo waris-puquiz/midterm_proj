ca77d6997e4ebab2df39bbcc7295fb4686e3f881
Ralph Lopez <ralphlopez5030@gmail.com>
2019-10-25 01:04:52 +0800

    ralph-version-1

---
+++ resource/php/function/borrow.php
@@ -0,0 +1,50 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/midterm/resource/php/db/config.php';
+error_reporting (E_ALL ^ E_NOTICE);
+class borrow extends config{
+public function borrowBook(){
+  $config = new config;
+  $pdo = $config->Connect();
+  $borrowid = $_GET['borrowid'];
+  $sql = "SELECT * FROM `book_tbl`";
+  $data = $pdo->prepare($sql);
+  $data->execute();
+  $results = $data->fetchAll();
+  foreach ($results as $result) {
+        $bookid = $result->book_id;
+        $qty = $result->qty;
+  }
+  if(isset($_GET['borrowid'])){
+    $qty--;
+    $sql = "UPDATE `book_tbl` SET `qty`= $qty WHERE `book_id` = $borrowid";
+    $data = $pdo->prepare($sql);
+    $data->execute();
+    if ($qty <= 0) {
+      $sql = "UPDATE `book_tbl` SET `qty`= 0 WHERE `book_id` = $borrowid";
+      $data = $pdo->prepare($sql);
+      $data->execute();
+    }
+  }
+  }
+
+public function returnBook(){
+    $config = new config;
+    $pdo = $config->Connect();
+    $returnid = $_GET['returnid'];
+    $sql = "SELECT * FROM `book_tbl`";
+    $data = $pdo->prepare($sql);
+    $data->execute();
+    $results = $data->fetchAll();
+    foreach ($results as $result) {
+          $bookid = $result->book_id;
+          $qty = $result->qty;
+    }
+    if(isset($_GET['returnid'])){
+      $qty++;
+      $sql = "UPDATE `book_tbl` SET `qty`= $qty WHERE `book_id` = $returnid";
+      $data = $pdo->prepare($sql);
+      $data->execute();
+    }
+    }
+}
+ ?>
--- sample.php
+++ sample.php
@@ -1,3 +1,9 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/midterm/resource/php/function/borrow.php';
+$borrow = new borrow;
+$borrow->borrowBook();
+$borrow->returnBook();
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +15,15 @@
   <link rel="stylesheet" href="resource/css/style.css">
   </head>
   <body>
-  <!-- start here -->
+    <?php
+    require_once $_SERVER['DOCUMENT_ROOT'].'/midterm/resource/php/function/view.php';
+    $view = new view;
+    if(isset($_GET['submit'])){
+    $view->viewAllCriteria();
+  }else{
+    $view->viewAllData();
+  }
+    ?>
   </body>
   </html>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
