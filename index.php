61fda3486a4bee6d7f6ccb79ab6e7830447b391e
Ralph Lopez <ralphlopez5030@gmail.com>
2019-10-25 01:21:14 +0800

    version-1

--- index.php
+++ index.php
@@ -6,43 +6,28 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title></title>
   <link rel="stylesheet" href="vendor/css/bootstrap.css">
-  <link rel="stylesheet" href="resource/css/style2.css">
+  <link rel="stylesheet" href="resource/css/style.css">
   </head>
   <body>
     <header>
       <nav class="navbar navbar-expand-sm navbar-light">
-        <div class="container justify-content-center">
+        <div class="container">
           <a class="navbar-brand" href="register.php"><IMG SRC="resource/img/logo1.png" ALT="Logo" WIDTH=210 HEIGHT=80></a>
-            </div>
-          </nav>
-    </header>
-    <section class="container-fluid">
+        </div>
+      </nav>
+    </header><section class="container-fluid">
       <section class="row justify-content-center mt-5">
         <section class="col-12 col-sm-6 col-md-3">
-          <div class="card">
-            <div class="card-header">Login</div>
+          <div class="card mt-5">
+            <div class="card-header">Home</div>
             <div class="card-body">
-              <form class="form-container" action="" method="post">
-        				<div class="form-row">
-        					<div class="form-group col-md-12">
-                    <label for="username">Username</label>
-                    <input type="text" name="username" id="username" class="form-control" autocomplete="off" required/>
-                  </div>
-        					<div class="form-group col-md-12">
-                    <label for="password">Password</label>
-                    <input type="password" name="password" id="password" class="form-control bg-light text-dark" autocomplete="off" required/>
-                  </div>
-        				</div>
-        				<div class="form-group">
-        					<input type="submit" value="Sign In" class="btn btn-primary btn-block" style="background-color: #2c296e;"/>
-        				</div>
-        			</form>
+              <li type="button"><a class="btn btn-primary btn-lg btn-block my-3" style="background-color: #2c296e;" href="login.php">Sign In</a></li>
+              <li type="button"><a class="btn btn-lg btn-block my-3 text-white" style="background-color: #ff0000;" href="register.php">Sign Up</a></li>
             </div>
           </div>
         </section>
       </section>
     </section>
-
 
 </body>
 </html>
--- register.php
+++ register.php
@@ -6,43 +6,30 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title></title>
   <link rel="stylesheet" href="vendor/css/bootstrap.css">
-  <link rel="stylesheet" href="resource/css/style.css">
+  <link rel="stylesheet" href="resource/css/style2.css">
   </head>
   <body>
     <header>
       <nav class="navbar navbar-expand-sm navbar-light">
-        <div class="container">
+        <div class="container justify-content-center">
           <a class="navbar-brand" href="register.php"><IMG SRC="resource/img/logo1.png" ALT="Logo" WIDTH=210 HEIGHT=80></a>
-            <button class="navbar-toggler bg-white" data-toggle="collapse" data-target="#navbarNav"><span class="navbar-toggler-icon"></span></button>
-              <div class="collapse navbar-collapse " id="navbarNav">
-                <ul class="navbar-nav ml-auto navbar-light">
-                  <form action="" method="GET" class="form-inline my-2 my-lg-0">
-                    <li class="nav-item"><a class="nav-link text-dark mr-3 mt-sm-3" href="login.php">Sign In</a></li>
-                  </form>
-                </ul>
-              </div>
             </div>
           </nav>
     </header>
-
     <section class="container-fluid">
       <section class="row justify-content-center mt-5">
         <section class="col-12 col-sm-6 col-md-3">
           <div class="card">
-            <div class="card-header">Register</div>
+            <div class="card-header">Add new user</div>
             <div class="card-body">
               <form class="form-container" action="" method="post">
                   <div class="form-row">
-                    <div class="form-group col-md-12">
-                      <label for="name">First name</label>
-                      <input type="text" class="form-control" id="name"required>
-                    </div>
                   <div class="form-group col-md-12">
                     <label for="username">Username</label>
                     <input type="text" name="username" id="username" class="form-control"" autocomplete="off>
                   </div>
                   <div class="form-group col-md-12">
-                    <label for="password">Choose a password</label>
+                    <label for="password">Password</label>
                     <input type="password" name="password" id="password" class="form-control bg-light text-dark" required>
                   </div>
                   <div class="form-group col-md-12">
@@ -51,7 +38,7 @@
                   </div>
                   </div>
                   <div class="form-group">
-                    <input type="submit" value="Sign Up" class="btn btn-block text-white" style="background-color: #2c296e;"/>
+                    <input type="submit" value="Sign Up" name="add" class="btn btn-block text-white" style="background-color: #2c296e;"/>
                   </div>
               </form>
             </div>
